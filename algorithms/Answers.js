920d48684df5ccf16720d741da18b19d9e9c9a27
Michael Marshal <mikemarshalman@gmail.com>
2018-05-04 12:33:34 -0500

    Exercise I complete

--- algorithms/Answers.js
+++ algorithms/Answers.js
@@ -1,3 +1,4 @@
+EXERCISE I:
 a)
 O(n) ... example: if n was 10
 while (a < 1000)
@@ -5,3 +6,32 @@ while (a < 1000)
 ... 10 iterations for a n of 10, thus time complexity is O(n)
 
 b)
+if (x > log(i)) {
+  the time complexity will be:
+  O(x)
+} else {
+  the time complexity will be:
+  O(log(i))
+}
+
+c)
+// O( 8+8+log(sqrt(n)) * 8+log(sqrt(n)) * log(sqrt(n)) )
+^ this is not the answer, at start:
+i = 0;
+sqrt(0) = 0, 0 / 2 = 0 ...
+0 is not less than 0, so the first iteration never starts
+O(0)
+
+d)
+O( n * log(n) )
+
+e)
+O((n-3+(n-2+10))*(n-2)*(n-1)*n)  AKA O(n^4)
+
+f)
+O(n)
+
+g)
+O(n)
+
+EXERCISE II:
