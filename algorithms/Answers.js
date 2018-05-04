e52f2fc93635c92ebbd57bf7e96e273693dae27e
Michael Marshal <mikemarshalman@gmail.com>
2018-05-04 11:52:47 -0500

    theory answers and algorithms started

---
+++ algorithms/Answers.js
@@ -0,0 +1,7 @@
+a)
+O(n) ... example: if n was 10
+while (a < 1000)
+  a += 100
+... 10 iterations for a n of 10, thus time complexity is O(n)
+
+b)
---
+++ theory/Answers.js
@@ -0,0 +1,12 @@
+// Regex
+
+// Antelopes
+ /(antelopes?)/g;
+
+ // Goat / Moat
+ /(g|m)(oat)/g;
+
+ // Dates
+ /\d+-\d{2}-\d{2}$/gm;
+
+ 
